b378f2bd095ace64889601fee51f088350d8ecd9
Sumit Gupta <sumitgupta28@gmail.com>
2025-08-04 12:49:14 -0500

    Init

--- src/main/java/com/openfeign/client/MovieApiFeignClient.java
+++ src/main/java/com/openfeign/client/MovieApiFeignClient.java
@@ -1,7 +1,6 @@
 package com.openfeign.client;
 
 import com.openfeign.entity.Movie;
-
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
